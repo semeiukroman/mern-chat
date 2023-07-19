ce9461e0f4f9903ead27d6e31e0c5bc95cec6b24
Roman <semeniukroman01@gmail.com>
2023-07-20 00:26:57 +0200

    first commit

---
+++ api/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const User = require('./models/user.js')
+
+dotenv.config();
+// mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    {}
+)
+    .then(() => console.log('connected'))
+    .catch(err => console.log(err));
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const port = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}))
+
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const createdUser = await User.create({ username, password });
+        jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json('ok');
+        });
+    } catch (err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Web app available at localhost:${port}/test`);
+});
+
+//zPyq3NN3BBb2MSvN
---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Register from './register';
+import axios from "axios";
+
+function App() {
+   axios.defaults.baseURL = 'http://localhost:4040';
+   axios.defaults.withCredentials = true;
+   return <Register />;
+}
+
+export default App;
